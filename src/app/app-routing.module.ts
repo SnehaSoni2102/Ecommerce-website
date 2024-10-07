769a91eac4aeaeeefcb843440902a070ef620cb6
Sneha Soni <snehasoni@Snehas-MacBook-Air.local>
2024-10-07 14:41:16 +0530

    Lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'ecommerce', loadChildren: () => import('./ecommerce/ecommerce.module').then(m => m.EcommerceModule) }, 
+{ path: 'orders', loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
